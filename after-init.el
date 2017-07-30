599304490c6a7234a287ce1601c535f0a603790f
Mohsen Tamiz <mohsen.tamiz@gmail.com>
2017-07-30 10:11:52 +0430

    Add a function to parse escape character and show its color, useful in reading some log files.

--- after-init.el
+++ after-init.el
@@ -362,3 +362,8 @@
 (setq js-indent-level 2)
 
 ;; start emacs server
+(require 'ansi-color)
+(defun display-ansi-colors ()
+  (interactive)
+  (let ((inhibit-read-only t))
+    (ansi-color-apply-on-region (point-min) (point-max))))
